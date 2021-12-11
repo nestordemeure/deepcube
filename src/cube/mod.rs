f6e5b5c3a52558c55fe604cc54d5d4294a34dc56
Nestor Demeure <nestordemeure@gmail.com>
2021-12-10 18:22:24 -0800

    moved cube identifier to another file

--- src/cube/mod.rs
+++ src/cube/mod.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 use enum_iterator::IntoEnumIterator;
 use rand::seq::SliceRandom;
 pub mod sizes;
-mod color;
+pub mod color;
 mod moves;
 pub mod coordinates;
 mod display;
@@ -10,8 +10,6 @@ pub use color::Color;
 pub use sizes::{NB_FACES, NB_SQUARES_CUBE, NB_SQUARES_FACE, NB_SQUARES_SIDE};
 pub use moves::Move;
 pub use coordinates::{Face, Coordinate1D, Coordinate2D, RotationAxis};
-
-use self::color::NB_COLORS;
 
 //-----------------------------------------------------------------------------
 // Cube
@@ -111,58 +109,6 @@ impl Cube
         {
             let random_move = moves.choose(&mut rng).unwrap();
             result = result.apply_move(random_move);
-        }
-        result
-    }
-
-    /// converts the cube into a unique identifier
-    /// we use the colors of the center squares to put the cube in standard orientation
-    /// we do not encode the center square as it is always of the same color
-    /// we do not encode the last face as it is entirely deifned by the other faces
-    pub fn to_identifier(&self) -> u128
-    {
-        // constants with the proper types
-        let nb_colors_u128 = NB_COLORS as u128;
-        let nb_squares_per_face = (NB_SQUARES_FACE - 1) as u32; // we ignore the center square
-        let index_face_ignored = (NB_FACES - 1) as u32; // the last face is not encoded
-
-        // looping on all faces
-        let mut result: u128 = 0;
-        for index_face in 0..NB_FACES
-        {
-            // extracts the face that we are currently checking
-            let start_index = index_face * NB_SQUARES_FACE;
-            let end_index = start_index + NB_SQUARES_FACE;
-            let face = &self.squares[start_index..end_index];
-
-            // gets the index of the face using its center square
-            let rotated_index_face = face[4] as u32;
-            // we do not encode the last face as it can be deduced from the other faces
-            if rotated_index_face == index_face_ignored
-            {
-                continue;
-            }
-
-            // computes a code uniquely identifying the face
-            let mut result_face = 0;
-            // 4 squares before center square
-            for i in 0..4
-            {
-                let color_index = face[i] as usize;
-                result_face = result_face * NB_COLORS + color_index;
-            }
-            // 4 squares after center square
-            for i in 5..NB_SQUARES_FACE
-            {
-                let color_index = face[i] as usize;
-                result_face = result_face * NB_COLORS + color_index;
-            }
-
-            // computes the multiplication factor uniquely identifying the center color
-            let face_shift = nb_colors_u128.pow(nb_squares_per_face * rotated_index_face);
-
-            // assemble
-            result += (result_face as u128) * face_shift;
         }
         result
     }
