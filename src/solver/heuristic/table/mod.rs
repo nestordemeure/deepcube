1fb0797ce42460e4fc5dba969f929c4ee2682eb4
Nestor Demeure <nestordemeure@gmail.com>
2021-12-18 17:24:16 -0800

    made parallel the default for heuristic generation

--- src/solver/heuristic/table/mod.rs
+++ src/solver/heuristic/table/mod.rs
@@ -34,121 +34,8 @@ impl<E: Encoder> Heuristic for TableHeuristic<E>
 
 impl<E: Encoder + Sync> TableHeuristic<E>
 {
-    //---------------------------------------------------------------------------------------------
-    // PARALLEL
-
-    /// initialize the heuristic
+    /// initialize the heuristic in parallel
     pub fn new() -> TableHeuristic<E>
-    {
-        // initializes the table and encoder
-        let encoder = E::new();
-        let table_size = E::nb_indexes();
-        let mut table = vec![None; table_size];
-
-        // progress bar to track progress
-        let mut progress_bar = Bar::with_range(0, table_size).timed();
-        let mut timer = Stopwatch::start_new();
-
-        // uses an iterative deepening search to fill the table
-        let solved_cubes = Cube::all_solved_cubes();
-        let moves = Move::all_moves();
-        let mut current_table_size = 0;
-        // depth left when exploring the various cubes
-        let mut depth_cubes = vec![-1; table_size];
-        for depth in 0..
-        {
-            // iterates at the given depth from all solved cubes
-            let mut nb_new_cubes = 0;
-            for cube in solved_cubes.iter()
-            {
-                Self::iterative_deepening(cube,
-                                          &moves,
-                                          &mut depth_cubes,
-                                          &mut table,
-                                          &mut nb_new_cubes,
-                                          &encoder,
-                                          0,
-                                          depth);
-            }
-
-            // displays the current result
-            current_table_size += nb_new_cubes;
-            if current_table_size == table_size
-            {
-                // display final informations on the table
-                timer.stop();
-                println!("Table done! (maximum distance:{} table size:{} computing time:{:?})",
-                         depth,
-                         table_size,
-                         timer.elapsed());
-                break;
-            }
-            else
-            {
-                // displays information on the current depth
-                progress_bar.set(current_table_size);
-                println!("Table: did distance {} {} in {:?}", depth, progress_bar, timer.elapsed());
-            }
-        }
-
-        // removes the options
-        let table = table.into_iter()
-                         .map(|distance_option| distance_option.expect("index was never encountered!"))
-                         .collect();
-        TableHeuristic { encoder, table }
-    }
-
-    /// registers all new cubes at depth max_depth
-    fn iterative_deepening(cube: &Cube,
-                           moves: &[Move],
-                           depth_cubes: &mut [i8],
-                           table: &mut [Option<u8>],
-                           nb_new_cubes: &mut usize,
-                           encoder: &E,
-                           depth: u8,
-                           max_depth: u8)
-    {
-        // avoids running code on cubes whose children are all known
-        let index = encoder.encode(cube);
-        let depth_left = (max_depth - depth) as i8;
-        if depth_cubes[index] < depth_left
-        {
-            // registers cube
-            depth_cubes[index] = depth_left;
-
-            if depth == max_depth
-            {
-                // we are at the depth limit, registers the depth
-                if table[index].is_none()
-                {
-                    table[index] = Some(depth);
-                    *nb_new_cubes += 1;
-                }
-            }
-            else
-            {
-                // goes further in depth
-                for m in moves.iter()
-                {
-                    let child_cube = cube.apply_move(m);
-                    Self::iterative_deepening(&child_cube,
-                                              moves,
-                                              depth_cubes,
-                                              table,
-                                              nb_new_cubes,
-                                              encoder,
-                                              depth + 1,
-                                              max_depth);
-                }
-            }
-        }
-    }
-
-    //---------------------------------------------------------------------------------------------
-    // PARALLEL
-
-    /// initialize the heuristic
-    pub fn new_parallel() -> TableHeuristic<E>
     {
         // initializes the table and encoder
         let encoder = E::new();
@@ -171,14 +58,14 @@ impl<E: Encoder + Sync> TableHeuristic<E>
             let nb_new_cubes = AtomicUsize::new(0);
             solved_cubes.par_iter().for_each(|cube| {
                                        let mut nb_new_cubes_thread = 0;
-                                       Self::iterative_deepening_parallel(cube,
-                                                                          &moves,
-                                                                          &depth_cubes,
-                                                                          &table,
-                                                                          &mut nb_new_cubes_thread,
-                                                                          &encoder,
-                                                                          0,
-                                                                          depth);
+                                       Self::iterative_deepening(cube,
+                                                                 &moves,
+                                                                 &depth_cubes,
+                                                                 &table,
+                                                                 &mut nb_new_cubes_thread,
+                                                                 &encoder,
+                                                                 0,
+                                                                 depth);
                                        nb_new_cubes.fetch_add(nb_new_cubes_thread, Ordering::Relaxed);
                                    });
             let nb_new_cubes = nb_new_cubes.into_inner();
@@ -210,19 +97,18 @@ impl<E: Encoder + Sync> TableHeuristic<E>
         // removes the options
         let table: Vec<u8> =
             table.into_par_iter().map(|atomic_distance| atomic_distance.into_inner()).collect();
-        assert!(table.par_iter().all(|distance| *distance != u8::MAX));
         TableHeuristic { encoder, table }
     }
 
     /// registers all new cubes at depth max_depth
-    fn iterative_deepening_parallel(cube: &Cube,
-                                    moves: &[Move],
-                                    depth_cubes: &[AtomicI8],
-                                    table: &[AtomicU8],
-                                    nb_new_cubes: &mut usize,
-                                    encoder: &E,
-                                    depth: u8,
-                                    max_depth: u8)
+    fn iterative_deepening(cube: &Cube,
+                           moves: &[Move],
+                           depth_cubes: &[AtomicI8],
+                           table: &[AtomicU8],
+                           nb_new_cubes: &mut usize,
+                           encoder: &E,
+                           depth: u8,
+                           max_depth: u8)
     {
         // avoids running code on cubes whose children are all known
         let index = encoder.encode(cube);
@@ -250,16 +136,24 @@ impl<E: Encoder + Sync> TableHeuristic<E>
                 for m in moves.iter()
                 {
                     let child_cube = cube.apply_move(m);
-                    Self::iterative_deepening_parallel(&child_cube,
-                                                       moves,
-                                                       depth_cubes,
-                                                       table,
-                                                       nb_new_cubes,
-                                                       encoder,
-                                                       depth + 1,
-                                                       max_depth);
+                    Self::iterative_deepening(&child_cube,
+                                              moves,
+                                              depth_cubes,
+                                              table,
+                                              nb_new_cubes,
+                                              encoder,
+                                              depth + 1,
+                                              max_depth);
                 }
             }
         }
+    }
+}
+
+impl<E: Encoder + Sync> Default for TableHeuristic<E>
+{
+    fn default() -> Self
+    {
+        Self::new()
     }
 }
